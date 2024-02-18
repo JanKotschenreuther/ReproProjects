271e530714bbaa731efd3f4487c60d1ce5870ff0
JanKotschenreuther <JanKotschenreuther@users.noreply.github.com>
2024-02-18 15:14:50 +0100

    Add files via upload

---
+++ BlazorHybridApp/Publish.ps1
@@ -0,0 +1,9 @@
+﻿dotnet publish .\adv-dbupdater-ui.csproj `
+    -o C:\Publish\adv-dbupdater-ui `
+    -c Release `
+    -f net8.0-windows10.0.19041.0 `
+    -p:WindowsAppSDKSelfContained=true `
+    -p:WindowsPackageType=None `
+    -p:RuntimeIdentifierOverride=win10-x64 `
+    -p:ErrorOnDuplicatePublishOutputFiles=true `
+    -p:Version=1.0.8
